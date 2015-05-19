f56dc746448665f0c485bc6cc5d360660d9a529d
dannyacv10 <dannyacv10@javerianacali.edu.co>
2015-05-18 19:51:36 -0500

    funciones movimientos bola

--- funciones.py
+++ funciones.py
@@ -1,17 +1,22 @@
 import pprint
 
-comandospro1= """
+prg =""" 
 avanzar
 retroceder
 subir
+avanzar
+avanzar
+avanzar
 bajar
+subir
+avanzar
+subir
+retroceder
 """
-
 
 ##Espacio
 
-
-esp= []
+esp = []
 for i in range(12):
 	esp.append([0]*24)
 	
@@ -23,31 +28,38 @@ pp.pprint(esp)
 ## Fin espacio
 ##Funcion espacio y programa
 
-def jugar (esp,pro):
-	#0: avanzar 
-	#1: retroceder 
-	#2: subir 
-	#3: bajar
-	dire=raw_input("indique la direccion de la bola entre 0 hasta 3: ")
-	print dire  
-	
-	dire=(dire)
-	
-	pos=raw_input("indique la posicion de la bola entre (0,0) hasta (12,0): ")
-	print pos  
-	
+def jugar (esp,programa):
+	dire=0 #mirando de frente
+	print("comienza en la direccion",dire)
+	pos=input("posicion en que comienza la bola debe ser entre (0,0) hasta (12,0):" )
 	pos=(pos)
+
+	print("comienza en la posicion",pos)
 	
-	l= pro.split("/n")
-	print(l)
-	for comando in l:
-		print(comando)
-		if comando == "avanzar":
+	lineas= programa.split("\n")
+	for cmd in lineas:
+		print(cmd)
+		if cmd == "avanzar":
+			if dire==0: #Mirando de frente 
+				pos=((pos)[0],(pos)[1]+1)
+			print("nueva posicion bola",pos)
+			
+		if cmd=="retroceder":
+			if dire==0: #Mirando de frente
+				pos=((pos)[0],(pos)[1]-1)
+			print("nueva posicion bola",pos)
+			
+		if cmd=="subir":
+			if dire==0: 
+				pos=((pos)[0]-1,(pos)[1])
+			print("nueva pos",pos)
+			
+		if cmd=="bajar":
 			if dire==0:
 				pos=((pos)[0]+1,(pos)[1])
-			
+			print("nueva posicion bola",pos)	
+		
 
-
-jugar(esp,comandospro1)
-
+jugar(esp,prg)
+pp.pprint(esp)
 
